798d01ab4fffd229c9ec2772e4ac3e45163aacbd
Sylvain FARNAULT <sylvain.farnault@gmail.com>
2020-09-21 10:35:37 +0200

    starting project

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
-  get "power", to: "pages#power"
-  get "power/seasons", to: "seasons#index"
+  get "power", to: "pages#power" do
+    get "seasons", to: "seasons#index"
+  end
 
 end
