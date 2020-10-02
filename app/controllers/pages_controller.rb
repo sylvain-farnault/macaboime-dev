c4743f5b23605d67d7e041616f93621d6d6fdf86
Sylvain FARNAULT <sylvain.farnault@gmail.com>
2020-10-02 15:40:56 +0200

    wip

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
   def home
+    current_year = Time.now.year
+    start_year =
+      (Time.now > Time.new(current_year,8,15)) ?
+      current_year : (current_year - 1)
+    season_years = start_year.to_s + "-" + (start_year + 1).to_s
+    season = Season.where(years: season_years)
+    competition = Competition.where(kind: "championship")
+    @current_championship_edition = Edition.where(season: season, competition: competition)
+    raise
   end
 
   def power
