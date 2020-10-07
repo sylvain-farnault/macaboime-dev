54f16a3e6bb6be799b905618d44e3f5f496a4d38
Sylvain FARNAULT <sylvain.farnault@gmail.com>
2020-10-07 12:16:42 +0200

    basic calendar display - 2 first schedules

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
+    # GET current championship edition instance that need to be send to the partial
+    # See later the possibility to get season_years into path macaboime.fr/2020-2021
     current_year = Time.now.year
     start_year =
       (Time.now > Time.new(current_year,8,15)) ?
@@ -7,8 +9,7 @@ class PagesController < ApplicationController
     season_years = start_year.to_s + "-" + (start_year + 1).to_s
     season = Season.where(years: season_years)
     competition = Competition.where(kind: "championship")
-    @current_championship_edition = Edition.where(season: season, competition: competition)
-    raise
+    @current_championship_edition = Edition.find_by(season: season, competition: competition)
   end
 
   def power
