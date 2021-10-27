0135e83e833b3f9e55066dba4a8a14d886be4c57
Sylvain FARNAULT <sylvain.farnault@gmail.com>
2021-10-28 01:42:50 +0200

    Create custom User missing_method to respond to admin? player? (all roles)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :user_roles
+  has_many :roles, through: :user_roles
+
+  def method_missing(m, *args, &block)
+    if Role.pluck(:label).include?(m.to_s.gsub('?', '')) && m[-1] == '?'
+      self.roles.pluck(:label).include?(m.to_s.gsub('?', '')) ? true : false
+    else
+      super
+    end
+  end
 end
