87005b2c59b234b51a960f620ad41d37e58e4b42
Sylvain FARNAULT <sylvain.farnault@gmail.com>
2021-10-29 18:01:03 +0200

    user_role validation for :role to get message on save errors

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -4,4 +4,5 @@ class UserRole < ApplicationRecord
 
   validates_presence_of :user, :role
   validates :user, uniqueness: { scope: :role, message: "can\'t have twice the same role!" }
+  validates :role, uniqueness: { scope: :user, message: ": This user is already an %{value}" }
 end
