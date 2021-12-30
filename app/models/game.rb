a1ade472e1fa37bdad4122c074ce156121fef3b8
Sylvain FARNAULT <sylvain.farnault@gmail.com>
2021-12-30 23:02:50 +0100

    Optimise enter_results page for external referee intervention

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,6 +4,7 @@ class Game < ApplicationRecord
 
   has_many :results
   accepts_nested_attributes_for :results
+  default_scope { order(:id) }
 
   # validates :stadium, uniqueness: { allow_blank: true, allow_nil: true }
   validates :schedule, presence: true
