118c09b6136b3aeecdf376940bd9ca121d43ee50
Sylvain Farnault <59399139+sylvain-farnault@users.noreply.github.com>
2024-12-20 21:44:53 +0100

    I am not in control about Bootstrap initialization: after commenting everything it still work!

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,8 +29,8 @@ import { markControlsHandler } from '../components/mark_controls_handler'
 import { forfeitBtnHandler } from '../components/forfeit_btn_handler'
 
 // require("jquery");
-import "@popperjs/core"
-import "bootstrap";
+// import "@popperjs/core"
+//import "bootstrap";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
