72c2dbcb78036f031884f25c870207a5f6077384
Sylvain Farnault <59399139+sylvain-farnault@users.noreply.github.com>
2024-12-20 21:45:43 +0100

    Changing the way rails/ujs is initialized but finally do not use it. Should be removed ??

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,10 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-require("@rails/ujs").start()
+import "@rails/ujs"
+import Rails from "@rails/ujs"
+Rails.start()
+
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
